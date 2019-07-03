3e9704db3ce8512a5debc07a60846de3a7393266
Jens Schauder <jschauder@pivotal.io>
2019-07-03 14:26:50 +0200

    Error reproduced

---
+++ README.md
@@ -0,0 +1,11 @@
+# Reproduces an issue with IN-clauses by running the following build:
+
+run
+
+```
+mvn test
+```
+
+to see the tests failing.
+
+EclipseLink issue: TBD which is a follow up on https://bugs.eclipse.org/bugs/show_bug.cgi?id=349477
---
+++ src/test/java/sample/data/jpa/SimpleEntityTest.java
@@ -0,0 +1,105 @@
+package sample.data.jpa;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.ParameterExpression;
+import javax.persistence.criteria.Root;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+/**
+ * Integration test to run the application.
+ *
+ * @author Oliver Gierke
+ * @author Jens Schauder
+ */
+public class SimpleEntityTest {
+
+	private static final String SELECT_WITH_IN_CLAUSE = "SELECT se FROM SimpleEntity se WHERE se.id IN :ids";
+	private static final String SELECT_WITH_IN_CLAUSE_IN_PARENS = "SELECT se FROM SimpleEntity se WHERE se.id IN (:ids)";
+
+	EntityManager em;
+
+
+	@Before
+	public void before() {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("simple");
+		em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		storeEntity(23L);
+		storeEntity(42L);
+	}
+
+	@After
+	public void after() {
+
+		em.getTransaction().rollback();
+	}
+
+	@Test
+	public void inCriteriaApiWithNonEmptyListDirectParameterSetting() {
+
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<SimpleEntity> criteriaQuery = cb.createQuery(SimpleEntity.class);
+		Root<SimpleEntity> root = criteriaQuery.from(SimpleEntity.class);
+		criteriaQuery.select(root).where(root.get("id").in(cb.parameter(Collection.class, "ids")));
+
+		TypedQuery<SimpleEntity> query = em.createQuery(criteriaQuery);
+
+		List<SimpleEntity> result = query
+				.setParameter("ids", Arrays.asList(23L, 42L))
+				.getResultList();
+
+		assertThat(result).hasSize(2);
+	}
+
+	@Test
+	public void inCriteriaApiWithNonEmptyList() {
+
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<SimpleEntity> criteriaQuery = cb.createQuery(SimpleEntity.class);
+		Root<SimpleEntity> root = criteriaQuery.from(SimpleEntity.class);
+		ParameterExpression<Iterable> parameter1 = cb.parameter(Iterable.class);
+		criteriaQuery.select(root).where(root.get("id").in(parameter1));
+
+		TypedQuery<SimpleEntity> typedQuery = em.createQuery(criteriaQuery);
+
+
+		Set<ParameterExpression<?>> parameters = criteriaQuery.getParameters();
+		assertThat(parameters).hasSize(1);
+
+		for (ParameterExpression parameter : parameters) {
+			typedQuery.setParameter((ParameterExpression<Iterable>)parameter, Arrays.asList(23L, 42L));
+		}
+
+
+		List<SimpleEntity> result = typedQuery
+				.getResultList();
+
+		assertThat(result).hasSize(2);
+	}
+
+	private void storeEntity(long id) {
+
+		SimpleEntity entity = new SimpleEntity();
+		entity.setId(id);
+		em.persist(entity);
+		em.flush();
+	}
+
+}
